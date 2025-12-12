2ca0c8280df588a52764b41d30380c8958e69950
jason0320 <jasonnosaj0320@gmail.com>
2025-12-12 16:47:01 +0800

    加入專案檔案。

---
+++ Mod_PandaUmbrellaPackMod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+
+namespace PandaUmbrellaMod
+{
+    [BepInPlugin("panda.umbrealla.mod", "Panda's Umbrella Mod", "1.0.0.0")]
+
+    public class Mod_PandaUmbrellaPackMod : BaseUnityPlugin
+    {
+        private void OnStartCore()
+        {
+            string pathToExcelFile = Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location) + "/Umbrella.xlsx";
+            SourceManager sources = Core.Instance.sources;
+            ModUtil.ImportExcel(pathToExcelFile, "Thing", sources.things);
+        }
+    }
+}
